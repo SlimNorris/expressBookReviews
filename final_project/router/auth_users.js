e6304a1671fd680c8d1a005d66ed143512cbef6a
Brandon Islas <104156453+SlimNorris@users.noreply.github.com>
2023-07-04 02:07:33 -0300

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -25,6 +25,25 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+  
+    if (!isbn || !username) {
+      return res.status(400).json({ message: "Invalid request. ISBN and username are required." });
+    }
+  
+    const book = books[isbn];
+  
+    if (!book || !book.reviews || !book.reviews[username]) {
+      return res.status(404).json({ message: "Review not found." });
+    }
+  
+    delete book.reviews[username];
+    return res.status(200).json({ message: "Review deleted successfully." });
+  });
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
+
